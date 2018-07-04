a64fe0b66b2782f04d508127c7baa93a89ffd87a
Edward Tadros <etadros@proticom.com>
2018-07-04 07:51:36 -0700

    Add logistic_regression.py (lecture 173)

---
+++ Get the Machine Learning basics/Classification Template/logistic_regression.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  4 07:42:38 2018
+
+@author: edward
+"""
+
+# Logistic Regression
+
+# Importing the Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv("Social_Network_Ads.csv")
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, 4].values
+
+# Splitting the dataset into the Training set and the Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
