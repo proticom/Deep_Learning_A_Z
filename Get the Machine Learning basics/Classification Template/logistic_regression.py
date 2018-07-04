824bcad247d922609bb85a51c0a0eb4368a11e0c
Edward Tadros <etadros@proticom.com>
2018-07-04 07:57:51 -0700

    Add prediction classifier (lecture 175)

--- Get the Machine Learning basics/Classification Template/logistic_regression.py
+++ Get the Machine Learning basics/Classification Template/logistic_regression.py
@@ -32,3 +32,6 @@ X_test = sc_X.transform(X_test)
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
